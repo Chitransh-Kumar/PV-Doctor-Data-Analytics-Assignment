0599874d33507c3fc82180158f807e5a99ae90d4
Chitransh-Kumar <kumar.chitransh1103@gmail.com>
2026-02-01 16:22:27 +0530

    Changed python files

--- data_collection.py
+++ data_collection.py
@@ -10,45 +10,35 @@ def data_preprocessing(base_data_path, output_csv):
     :param output_csv: Name of desired output .csv file
     """
 
-    # Sets file path for PR and GHI folders 
     pr_root_path= os.path.join(base_data_path, "PR")
     ghi_root_path= os.path.join(base_data_path, "GHI")
 
-    # List where the data is to be saved
     records= []
 
-    # Loop through all PR folders in ascending order
     for month in sorted(os.listdir(pr_root_path)):
         
-        # Sets file path for each month in PR and GHI folder
         pr_month_path= os.path.join(pr_root_path, month)
         ghi_month_path= os.path.join(ghi_root_path, month)
 
-        # If there is no month for a specific year then skip that month
         if not os.path.isdir(pr_month_path):
             continue
 
         if not os.path.isdir(ghi_month_path):
             continue
 
-        # Loop through all PR folders for each month in ascending order
         for file in sorted(os.listdir(pr_month_path)):
 
-            # Sets file path for each 5-day .csv file in PR and GHI folder
             pr_file_path= os.path.join(pr_month_path, file)
             ghi_file_path= os.path.join(ghi_month_path, file)
 
-            # Each PR and GHI .csv file is accessed for each year, month and date (5-day)
             pr_df= pd.read_csv(pr_file_path)
             ghi_df= pd.read_csv(ghi_file_path)
 
             pr_date= pd.to_datetime(pr_df.iloc[:, 0])
             
-            # Only numerical values is selected from PR and GHI .csv files
             pr_values= pr_df.select_dtypes(include= "number").iloc[:, 0]
             ghi_values= ghi_df.select_dtypes(include= "number").iloc[:, 0]
 
-            # For each date, Date, GHI value and PR value is added in the python list
             for date, pr_value, ghi_value in zip(pr_date, pr_values, ghi_values):
                 records.append({
                     "Date": date.date(),
@@ -56,13 +46,10 @@ def data_preprocessing(base_data_path, output_csv):
                     "PR": pr_value,
                 })
 
-    # Converts python list to Pandas DataFrame object
     final_df= pd.DataFrame(records)
 
-    # Sorts the dataframe by Date column for double-check
     final_df.sort_values("Date", inplace= True)
     
-    # Saves the output .csv file
     final_df.to_csv(output_csv, index= False)
 
     return final_df
--- data_visualization.py
+++ data_visualization.py
@@ -12,11 +12,8 @@ def data_visualization(csv_path, start_date= None, end_date= None):
     
     df= pd.read_csv(csv_path)
 
-    # Converts Date column data-type from Object (string) to Date.
     df['Date']= pd.to_datetime(df['Date'])
 
-
-    # If start and end date is mentioned then filter the data accordingly
     if start_date is not None:
         start_date= pd.to_datetime(start_date)
         df= df[df['Date']>=start_date]
@@ -25,7 +22,7 @@ def data_visualization(csv_path, start_date= None, end_date= None):
         end_date= pd.to_datetime(end_date)
         df= df[df['Date']<=end_date]
 
-    # GHI values color mapping
+
     def ghi_color(ghi):
         if ghi<2:
             return "navy"
@@ -38,11 +35,8 @@ def data_visualization(csv_path, start_date= None, end_date= None):
     
     df['GHI_Color']= df['GHI'].apply(ghi_color)
 
-    # 30-day moving average
     df['PR_30D_MA']= df['PR'].rolling(window= 30).mean()
 
-    # Budget year calculation 
-    # Year starts from July and ends at June
     def budget_year_index(date):
         if date.month >=7:
             return date.year - 2019
@@ -51,32 +45,23 @@ def data_visualization(csv_path, start_date= None, end_date= None):
 
     df['Budget_year']= df['Date'].apply(budget_year_index).astype(int)
 
-    # Values as specified in the assignment
     BASE_BUDGET= 73.9 
     YEARLY_DROP= 0.8 
 
     df['Budget_PR']= BASE_BUDGET - (df['Budget_year'] * YEARLY_DROP)
 
-
-    # Points above target budget PR
     above_budget= df[df['PR']> df['Budget_PR']]
     total_count_percent= round((len(above_budget) / len(df)) * 100, 2)
 
-
-    # Graph plotting
     plt.figure(figsize= (14, 7))
 
-    # Scatter plot for PR values with color encoding of corresponding GHI values
     plt.scatter(df['Date'], df['PR'], c= df['GHI_Color'], s= 20, alpha= 0.8, label= 'Daily PR')
 
-    # Plot for 30 day moving average of PR values
     plt.plot(df['Date'], df['PR_30D_MA'], color= 'red', linewidth= 2, label= '30 day moving avg (PR)')
 
-    # Plot for Target Budget PR
     plt.plot(df['Date'], df['Budget_PR'], color= 'darkgreen', linewidth= 2, label= 'Target Budget PR')
 
 
-    # Summary statistics
     last_7= df['PR'].tail(7).mean()
     last_30= df['PR'].tail(30).mean()
     last_60= df['PR'].tail(60).mean()
@@ -104,7 +89,6 @@ def data_visualization(csv_path, start_date= None, end_date= None):
         horizontalalignment= "right"
     )
 
-    # Above budget PR statistics text
     min_pr= df['PR'].min()
     mid_date= df['Date'].iloc[len(df)//2]
 
@@ -116,8 +100,6 @@ def data_visualization(csv_path, start_date= None, end_date= None):
         fontsize= 12
     )
 
-
-    # Graph formatting
     plt.xlabel("Date")
     plt.ylabel("Performance Ratio (%) ")
     plt.title("Performance Ratio Evaluation")
